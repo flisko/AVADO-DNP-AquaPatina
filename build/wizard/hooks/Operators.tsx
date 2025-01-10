967df4ac8a97ad66421ea50a7124e83c17a21cb6
flisko <ziga.flis@gmail.com>
2025-01-10 13:18:02 +0100

    chore: fix for assigned validators
    The SSV API doesnt return the validator index, so it was displaying the wrong assigned validators.
    
    Quick fix was to display the validator pub keys. Also prepared an endpoint which returns the validator index based on the pub_key, if we want to display that instead of the pub key later.

--- build/wizard/hooks/Operators.tsx
+++ build/wizard/hooks/Operators.tsx
@@ -1,69 +1,66 @@
-import useSWR from "swr";
 import axios from "axios";
+import useSWR from "swr";
+import { server_config } from "../config";
 import { OperatorType, ValidatorType } from "../types";
-import { server_config } from "../config"
 
 function get(api_url: string) {
-    const fetcher = async (url: string) => await axios.get(url).then((res) => res.data);
-    const { data, error } = useSWR(api_url, fetcher);
-    return { data: data?.data, error: error };
+  const fetcher = async (url: string) =>
+    await axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR(api_url, fetcher);
+  return { data: data?.data, error: error };
 }
 
-export function useOperatorOwnedBy({
-    address,
-}: {
-    address?: string,
-}) {
-    const api_url: string = `${server_config.monitor_url}/operators/owned_by/${address}`;
-    const { data, error } = get(api_url)
-    return { data: data?.operators[0] as OperatorType, error: error };
+export function useOperatorOwnedBy({ address }: { address?: string }) {
+  const api_url: string = `${server_config.monitor_url}/operators/owned_by/${address}`;
+  const { data, error } = get(api_url);
+  return { data: data?.operators[0] as OperatorType, error: error };
 }
 
 export function useValidatorsInOperator({
-    operatorId,
+  operatorId,
 }: {
-    operatorId?: bigint,
+  operatorId?: bigint;
 }) {
-    const api_url: string = `${server_config.monitor_url}/validators/in_operator/${operatorId}`;
-    const { data, error } = get(api_url)
-    return { data: data?.validators as ValidatorType[], error: error };
+  const api_url: string = `${server_config.monitor_url}/validators/in_operator/${operatorId}`;
+  const { data, error } = get(api_url);
+  return { data: data?.validators as ValidatorType[], error: error };
 }
 
-export function useOperatorStatus({
-    operatorId,
-}: {
-    operatorId?: bigint,
-}) {
-    const api_url: string = `${server_config.monitor_url}/operators/${operatorId}`;
-    const { data, error } = get(api_url)
-    return { data: data as OperatorType, error: error };
+export function useOperatorStatus({ operatorId }: { operatorId?: bigint }) {
+  const api_url: string = `${server_config.monitor_url}/operators/${operatorId}`;
+  const { data, error } = get(api_url);
+  return { data: data as OperatorType, error: error };
 }
 
 export function useOperatorPublicKey() {
-    const api_url: string = `${server_config.monitor_url}/operatorPublicKey`;
-    const { data, error } = get(api_url)
-    return { data: data as string, error: error };
+  const api_url: string = `${server_config.monitor_url}/operatorPublicKey`;
+  const { data, error } = get(api_url);
+  return { data: data as string, error: error };
 }
 
 export function isRegistered() {
-    const api_url: string = `${server_config.monitor_url}/isRegistered`;
-    return get(api_url);
+  const api_url: string = `${server_config.monitor_url}/isRegistered`;
+  return get(api_url);
 }
 
 export function useOperatorId() {
-    const api_url: string = `${server_config.monitor_url}/operatorId`;
-    const { data, error } = get(api_url)
-    if (error)
-        console.log(error)
-    // console.log(data)
-    const operatorId = data ? BigInt(data) : undefined
-    return { data: operatorId, error: error };
+  const api_url: string = `${server_config.monitor_url}/operatorId`;
+  const { data, error } = get(api_url);
+  if (error) console.log(error);
+  // console.log(data)
+  const operatorId = data ? BigInt(data) : undefined;
+  return { data: operatorId, error: error };
 }
 
 export function useBeaconNodeStatus() {
-    const api_url: string = `${server_config.monitor_url}/beaconNodeStatus`;
-    const { data, error } = get(api_url)
-    if (error)
-        console.log(error)
-    return { data: data, error: error };
+  const api_url: string = `${server_config.monitor_url}/beaconNodeStatus`;
+  const { data, error } = get(api_url);
+  if (error) console.log(error);
+  return { data: data, error: error };
 }
+
+/*export function useValidatorIndex({ pubkey }: { pubkey?: bigint }) {
+  const api_url: string = `${server_config.monitor_url}/validatorIndex/${pubkey}`;
+  const { data, error } = get(api_url);
+  return { data: data as ValidatorResponseType, error: error };
+}*/
--- build/wizard/types.ts
+++ build/wizard/types.ts
@@ -1,42 +1,64 @@
 export type OperatorType = {
-    "id": number,
-    "id_str": string,
-    "declared_fee": number,
-    "previous_fee": number,
-    "fee": number,
-    "name": string,
-    "public_key": string,
-    "owner_address": string,
-    "address": string,
-    // "location": "",
-    // "setup_provider": "",
-    // "eth1_node_client": "",
-    // "eth2_node_client": "",
-    // "description": "",
-    // "website_url": "",
-    // "twitter_url": "",
-    // "linkedin_url": "",
-    // "logo": "",
-    // "type": "operator",
-    "performance": {
-        "24h": number,
-        "30d": number
-    },
-    // "is_valid": true,
-    // "is_deleted": false,
-    // "is_active": 0,
-    // "status": "Inactive",
-    "validators_count": number
-}
+  id: number;
+  id_str: string;
+  declared_fee: number;
+  previous_fee: number;
+  fee: number;
+  name: string;
+  public_key: string;
+  owner_address: string;
+  address: string;
+  // "location": "",
+  // "setup_provider": "",
+  // "eth1_node_client": "",
+  // "eth2_node_client": "",
+  // "description": "",
+  // "website_url": "",
+  // "twitter_url": "",
+  // "linkedin_url": "",
+  // "logo": "",
+  // "type": "operator",
+  performance: {
+    "24h": number;
+    "30d": number;
+  };
+  // "is_valid": true,
+  // "is_deleted": false,
+  // "is_active": 0,
+  // "status": "Inactive",
+  validators_count: number;
+};
 
 export type ValidatorType = {
-    "public_key": string,
-    "owner_address": string,
-    "status": string,
-    "operators": OperatorType[],
-    "validator_info": {
-        "index": number,
-        "status": string,
-        "activation_epoch": number
-    }
-}
+  public_key: string;
+  owner_address: string;
+  status: string;
+  operators: OperatorType[];
+  validator_info: {
+    index: number;
+    status: string;
+    activation_epoch: number;
+  };
+};
+
+export type ValidatorIndexType = {
+  pubkey: string;
+  withdrawal_credentials: string;
+  effective_balance: string;
+  slashed: boolean;
+  activation_eligibility_epoch: string;
+  activation_epoch: string;
+  exit_epoch: string;
+  withdrawable_epoch: string;
+};
+
+export type DataItemType = {
+  index: string;
+  balance: string;
+  status: string;
+  validator: ValidatorIndexType;
+};
+
+export type ValidatorResponseType = {
+  data: DataItemType[];
+};
